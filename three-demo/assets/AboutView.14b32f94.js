ec13368fedd4fe0219b736b9d6fb3270b89e2515
cccatmint-Y <cao9710@outlook.com>
2022-10-17 11:24:19 +0800

    CDN

--- three-demo/assets/AboutView.14b32f94.js
+++ three-demo/assets/AboutView.14b32f94.js
@@ -1,1 +1,2 @@
 import{openBlock as o,createElementBlock as r,createElementVNode as t}from"vue";import{_ as e}from"./index.e65fd848.js";import"vue-router";const n={},s={class:"about"},u=[t("h1",null,"This is an about page",-1)];const a=e(n,[["render",function(t,e){return o(),r("div",s,u)}]]);export{a as default};
+//# sourceMappingURL=AboutView.14b32f94.js.map
